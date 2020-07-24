fda7c68d1af0e61af24972b1e6566a0ab8a2e105
igor-blip <igorcs0800@gmail.com>
2020-07-23 22:02:57 -0300

    commonsJs

---
+++ src/modulos/moduloB.js
@@ -0,0 +1,3 @@
+module.exports = {
+    saudacao() { return 'Oi beleza?! eu sou o módulo B!' }
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+   // mode: 'development',
+    mode: 'production',
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {       
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                //'style-loader', //Add css a DOM injetando a tag <style>
+                'css-loader', // interpreta @import, url()...
+                'sass-loader',
+            ]           
+        }]
+    }
+}
